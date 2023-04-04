a7f433113e7911fed58a7500b343b8260a1290e5
Joane <joanealves2011@gmail.com>
2023-04-04 17:56:45 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  // public pesquisa: String = '';
+  public titulo: String = 'Meu primeiro App';
+  public imagemRandomica: string = 'https://source.unsplash.com/random/200x200';
 
-  constructor() {}
+  public atualiza(): void {
+    this.titulo = 'Texto alterado';
+  }
 
+  public acao(): void {
+    this.titulo = 'Texto modificado';
+  }
+
+  public resultado: String = '';
+
+  constructor(private navegacao: NavController) {}
+
+  ngOnInit() {}
+
+  abrirBotoes() {
+    this.navegacao.navigateForward('botoes');
+  }
+
+  abrirLista() {
+    this.navegacao.navigateForward('lista');
+  }
+
+  // recuperar() {
+  //   this.resultado = this.pesquisa;
+  // }
 }
